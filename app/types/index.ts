29b3b782a180cc3e9ed62dd5dab0663d23baf73a
Krystian <krystian.potaczek07@gmail.com>
2023-06-09 22:12:53 +0200

    Add invidual listing view.

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,8 +1,12 @@
-import { User } from "@prisma/client";
+import { Listing, User } from '@prisma/client';
+
+export type SafeListing = Omit<Listing, 'createdAt'> & {
+  createdAt: string;
+};
 
 export type SafeUser = Omit<
   User,
-  "createdAt" | "updatedAt" | "emailVerified"
+  'createdAt' | 'updatedAt' | 'emailVerified'
 > & {
   createdAt: string;
   updatedAt: string;
